03c56e9c10fd588396cd88e53a77c931a0e559ff
h4mxa <duke80800@gmail.com>
2023-12-30 22:56:18 +0500

    implemented jwt authentication

--- src/controllers/auth/login.controller.ts
+++ src/controllers/auth/login.controller.ts
@@ -1,7 +1,48 @@
 import { Request, Response } from "express";
+import userModel from "../../models/user/user.model";
+import { hashPassword } from "../../utils/methods";
+import jwt from "jsonwebtoken";
+import { getToken } from "../../utils/env";
 
 export class LoginController {
-  public loginUser(_: Request, response: Response) {
-    return response.send("user is login");
+  public async loginUser(request: Request, response: Response) {
+    try {
+      const { email, password } = request.body;
+
+      const user = await userModel.findOne({
+        email,
+      });
+
+      const encryptPassword = hashPassword(password);
+
+      if (user && encryptPassword === user.password) {
+        const token = jwt.sign(
+          {
+            userId: user._id,
+            email,
+          },
+          getToken(),
+          {
+            expiresIn: "8h",
+          }
+        );
+
+        return response.status(200).json({
+          userDetails: {
+            email,
+            userName: user.username,
+            token,
+          },
+        });
+      }
+
+      return response
+        .status(400)
+        .send("Invalid credentials. Please try again.");
+    } catch (error) {
+      return response
+        .status(500)
+        .send("Something went wrong. Please try again.");
+    }
   }
 }
--- src/controllers/auth/register.controller.ts
+++ src/controllers/auth/register.controller.ts
@@ -1,16 +1,50 @@
 import { Request, Response } from "express";
 import userModel from "../../models/user/user.model";
+import jwt from "jsonwebtoken";
+import { getToken } from "../../utils/env";
+import { hashPassword } from "../../utils/methods";
 
 export class RegisterController {
   public async registerUser(request: Request, response: Response) {
-    const user = await userModel.create({
-      username: request.body.username,
-      password: request.body.password,
-      email: request.body.email,
-    });
+    try {
+      const { username, email, password } = request.body;
 
-    console.log(user);
+      const userExists = await userModel.exists({ email });
 
-    return response.send("User has been added to database");
+      if (userExists) {
+        return response.status(409).send("E-mail is already in use");
+      }
+
+      const encryptPassword = hashPassword(password);
+
+      const user = await userModel.create({
+        username,
+        email: email.toLowerCase(),
+        password: encryptPassword,
+      });
+
+      const token = jwt.sign(
+        {
+          userId: user._id,
+          email,
+        },
+        getToken(),
+        {
+          expiresIn: "8h",
+        }
+      );
+
+      return response.status(201).json({
+        userDetails: {
+          email,
+          username,
+          token,
+        },
+      });
+    } catch (error) {
+      return response
+        .status(500)
+        .send(`Error occurred. Please try again ${error}`);
+    }
   }
 }
